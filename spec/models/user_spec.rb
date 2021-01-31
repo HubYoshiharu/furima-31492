242761b3b5006a58f7ccefb96d7813299cb838c8
HubYoshiharu <nakashima2017@yahoo.co.jp>
2021-01-31 22:24:11 +0900

    emailに@を含まない場合登録できないことのテストコード追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -29,6 +29,12 @@ RSpec.describe User, type: :model do
       expect(another_user.errors.full_messages).to include('Email has already been taken')
     end
 
+    it 'emailに@を含まない場合登録できないこと' do
+      @user.email = "testtest.com"
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Email is invalid")
+    end
+
     it 'passwordが空では登録できないこと' do
       @user.password = nil
       @user.valid?
