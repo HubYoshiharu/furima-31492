2fa084f151536f1ef6efd82a4709a5ae56dc3070
HubYoshiharu <nakashima2017@yahoo.co.jp>
2021-01-31 21:31:16 +0900

    Userモデルの単体テストコード実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,16 +6,16 @@ class User < ApplicationRecord
   has_many :items
   has_many :purchase_records
   with_options presence: true do
-    with_options format: { with: /\A[ぁ-んァ-ン一-龥々]/, message: "is invalid. Input full-width characters." } do
+    with_options format: { with: /\A[ぁ-んァ-ン一-龥々]/, message: 'is invalid. Input full-width characters.' } do
       validates :firstname
       validates :lastname
     end
-    with_options format: { with: /\A[ぁ-んァ-ン一-龥々]/, message: "is invalid. Input full-width kana characters." } do
+    with_options format: { with: /\A[ァ-ン一]/, message: 'is invalid. Input full-width kana characters.' } do
       validates :firstname_reading
       validates :lastname_reading
     end
     validates :nickname
     validates :birthday
   end
-  validates :password, format: { with: /\A[0-9a-z]+\z/i, message: "includes both letters and numbers." }
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'includes both letters and numbers.' }
 end
---
+++ spec/factories/users.rb
@@ -0,0 +1,14 @@
+FactoryBot.define do
+  factory :user do
+    nickname { Faker::Name.last_name }
+    email { Faker::Internet.free_email }
+    password = Faker::Internet.password(min_length: 6)
+    password { password }
+    password_confirmation { password }
+    firstname { Gimei.first.kanji }
+    lastname { Gimei.last.kanji }
+    firstname_reading { Gimei.first.katakana }
+    lastname_reading { Gimei.last.katakana }
+    birthday { Faker::Date.between(from: '1930-01-01', to: '2016-12-31') }
+  end
+end
