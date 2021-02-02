2155a9c99cff57233cc97f0f76b557be30f8cd09
HubYoshiharu <nakashima2017@yahoo.co.jp>
2021-02-02 20:38:42 +0900

    画像に関するバリデーションとその他バリデーションの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,16 +6,21 @@ class Item < ApplicationRecord
   belongs_to :category, :condition, :burden, :prefecture, :shipping_days
 
   with_options presence: true do
+    validates :image, unless: :was_attached?
     validates :item_name
     validates :description
-    with_options numericality: { other_than: 1 } do
+    with_options numericality: { other_than: 1, message: "can't be blank" } do
       validates :category_id
       validates :condition_id
       validates :burden_id
-      validates :prefecture
+      validates :prefecture_id
       validates :shipping_days_id
     end
-    validates :price
+    validates :price, numericality: {greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is out of range"}, format: {with: /\A[0-9]+\z/, message: "must be half-width characters"}
     validates :user
+  end
+
+  def was_attached?
+    self.image.attached?
   end
 end
