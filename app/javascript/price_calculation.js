9c84e283043bdb0f3e9229120d30c59aa7e47a9f
HubYoshiharu <nakashima2017@yahoo.co.jp>
2021-02-06 10:55:55 +0900

    price_calculation.jsの不要なコードを削除

--- app/javascript/price_calculation.js
+++ app/javascript/price_calculation.js
@@ -2,7 +2,6 @@ window.addEventListener('load', () => {
   const price = document.getElementById("item-price");
   price.addEventListener('input', () => {
     const inputValue = price.value;
-    console.log(inputValue);
     let fee = Math.floor(inputValue * 0.1);
     let profit = inputValue - fee;
     const addFeeDom = document.getElementById("add-tax-price");
