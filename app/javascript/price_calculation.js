eb17268a3e114932b0fc0177e70c303a75ee4f4b
HubYoshiharu <nakashima2017@yahoo.co.jp>
2021-02-07 11:27:31 +0900

    商品情報編集ページにて前データを保持したまま表示させる機能を追加

--- app/javascript/price_calculation.js
+++ app/javascript/price_calculation.js
@@ -1,5 +1,14 @@
 window.addEventListener('load', () => {
   const price = document.getElementById("item-price");
+  const inputValue = price.value;
+  let fee = Math.floor(inputValue * 0.1);
+  let profit = inputValue - fee;
+  const addFeeDom = document.getElementById("add-tax-price");
+  const addProfitDom = document.getElementById("profit");
+
+  addFeeDom.innerHTML = fee;
+  addProfitDom.innerHTML = profit;
+
   price.addEventListener('input', () => {
     const inputValue = price.value;
     let fee = Math.floor(inputValue * 0.1);
