6328b2069baa4baad9d08b915bd0418483cc3ff6
HubYoshiharu <nakashima2017@yahoo.co.jp>
2021-02-07 14:22:42 +0900

    編集画面でエラメッセージを表示させる/itemsコントローラのupdateアクション/ユーザー毎の状態遷移機能を実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :move_to_index, only: :edit
 
   def index
     @items = Item.includes(:user).order('created_at DESC')
@@ -26,6 +27,15 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path
+    else
+      render :edit
+    end
+  end
+
   private
 
   def item_params
@@ -40,5 +50,10 @@ class ItemsController < ApplicationController
       :shipping_days_id,
       :price
     ).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    @item = Item.find(params[:id])
+    redirect_to action: :index if current_user.id != @item.user.id || @item.purchase_record.present?
   end
 end
